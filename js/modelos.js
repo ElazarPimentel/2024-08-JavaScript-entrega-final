7f7f3c53377027388dac277e3e89f72e2a25e37b
Elazar Pimentel <elazar.pimentel@sent.com>
2024-07-18 22:40:07 -0300

    2024-07-18-2239 Agregando Luxon

---
+++ js/modelos.js
@@ -0,0 +1,54 @@
+/* Nombre del archivo: js/modelos.js
+Autor: Alessio Aguirre Pimentel
+Versión: 322 */
+
+// Clase para manejar los datos del cliente
+export class ClienteClass {
+    constructor(clienteId, clienteNombre, clienteTelefono) {
+        this.clienteId = clienteId || ClienteClass.generarId('cliente');
+        this.clienteNombre = clienteNombre;
+        this.clienteTelefono = clienteTelefono;
+        console.log('ClienteClass instance created:', this);
+    }
+
+    static generarId(prefijo) {
+        const id = `${prefijo}_` + Math.random().toString(36).substring(2, 15);
+        console.log(`generarId called with prefijo: ${prefijo}, generated id: ${id}`);
+        return id;
+    }
+}
+
+// Clase para manejar los datos de la mascota
+export class MascotaClass {
+    constructor(mascotaId, mascotaForeignClienteId, mascotaNombre, mascotaEdad) {
+        this.mascotaId = mascotaId || MascotaClass.generarId('mascota');
+        this.mascotaForeignClienteId = mascotaForeignClienteId;
+        this.mascotaNombre = mascotaNombre;
+        this.mascotaEdad = mascotaEdad;
+        console.log('MascotaClass instance created:', this);
+    }
+
+    static generarId(prefijo) {
+        const id = `${prefijo}_` + Math.random().toString(36).substring(2, 15);
+        console.log(`generarId called with prefijo: ${prefijo}, generated id: ${id}`);
+        return id;
+    }
+}
+
+// Clase para manejar los datos del turno
+export class TurnoClass {
+    constructor(turnoId, turnoForeignMascotaId, turnoFecha, turnoHora, turnoForeignServicioId) {
+        this.turnoId = turnoId || TurnoClass.generarId('turno');
+        this.turnoForeignMascotaId = turnoForeignMascotaId;
+        this.turnoFecha = turnoFecha;
+        this.turnoHora = turnoHora;
+        this.turnoForeignServicioId = turnoForeignServicioId;
+        console.log('TurnoClass instance created:', this);
+    }
+
+    static generarId(prefijo) {
+        const id = `${prefijo}_` + Math.random().toString(36).substring(2, 15);
+        console.log(`generarId called with prefijo: ${prefijo}, generated id: ${id}`);
+        return id;
+    }
+}
